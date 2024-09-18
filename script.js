9608a47f714885be6bc415ebe8d6dca28535ed5f
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-09-18 11:40:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getFormvalue() {
-	let form = document.getElementById('form1');
-	let formData = new FormData('form');
-    let fname =formData.get('fname');
+    let form = document.getElementById('form1');
+    let formData = new FormData(form); // Pass the form element, not a string
+    let fname = formData.get('fname');
     let lname = formData.get('lname');
     let fullName = fname + " " + lname;
     alert(fullName);
